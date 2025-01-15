d21017ddbeec6333ca8facb2a16a67713e15c0a4
mal359 <149554260+mal359@users.noreply.github.com>
2025-01-15 11:51:51 -0500

    AEG-WG 1.6

--- aes_wg/iz_aes_wg.h
+++ aes_wg/iz_aes_wg.h
@@ -1,7 +1,7 @@
 /*
-  Copyright (c) 1990-2015 Info-ZIP.  All rights reserved.
+  Copyright (c) 1990-2017 Info-ZIP.  All rights reserved.
 
-  See the accompanying file LICENSE.txt, version 2000-Apr-09 or later
+  See the accompanying file LICENSE, version 2000-Apr-09 or later
   for terms of use.  If this file is missing, the Info-ZIP license
   also may be found at:  ftp://ftp.info-zip.org/pub/infozip/license.html
 */
@@ -12,26 +12,26 @@
  */
 
 #ifndef __iz_aes_wg_h           /* Don't include more than once. */
-#define __iz_aes_wg_h
+# define __iz_aes_wg_h
 
-#ifndef IZ_AES_WG_BETA
+# ifndef IZ_AES_WG_BETA
 #  define IZ_AES_WG_BETA        /* This is a beta version. */
-#endif
+# endif
 
-#ifdef IZ_AES_WG_BETA
+# ifdef IZ_AES_WG_BETA
 #  undef IZ_AES_WG_BETA         /* This is not a beta version. */
-#endif
+# endif
 
 
-#define IZ_AES_WG_MAJORVER      1
-#define IZ_AES_WG_MINORVER      4
-#ifdef IZ_AES_WG_BETA
-#  define IZ_AES_WG_BETA_VER     "a BETA"
-#  define IZ_AES_WG_VERSION_DATE "23 Mar 2015"  /* Last real code change. */
-#else
-#  define IZ_AES_WG_BETA_VER     ""
-#  define IZ_AES_WG_VERSION_DATE "03 Apr 2015"  /* Last public release date. */
+# define IZ_AES_WG_MAJORVER     1
+# define IZ_AES_WG_MINORVER     6
+# ifdef IZ_AES_WG_BETA
+#  define IZ_AES_WG_BETA_VER      "a BETA"	
+#  define IZ_AES_WG_VERSION_DATE  "23 Jan 2017" /* Last real code change. */
+# else
+#  define IZ_AES_WG_BETA_VER      ""
+#  define IZ_AES_WG_VERSION_DATE  "23 Jan 2017" /* Last public release date. */
 #  define IZ_AES_WG_RELEASE
-#endif
+# endif
 
 #endif /* ndef __iz_aes_wg_h */
